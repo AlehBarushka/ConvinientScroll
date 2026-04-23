aa70e2c3dad1679e6741c7a9b3b886067104aaf6
Aleh Barushka <1.alehbarushka@gmail.com>
2026-04-23 10:20:30 +0300

    Initial Commit

---
+++ natural-scroll/natural_scrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  natural_scrollApp.swift
+//  natural-scroll
+//
+//  Created by aleh on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct natural_scrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
